5073c7e123214b420fb5e56eb6d6916c31221280
Paulo Medeiros <pauloricardodemedeiros@hotmail.com>
2016-05-16 17:15:56 -0300

    User_id to Items Table and Associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516194855) do
+ActiveRecord::Schema.define(version: 20160516200601) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
